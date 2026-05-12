321135bca47e269508592feea90c3f7adade767a
Sander Tonkens <43548774+stonkens@users.noreply.github.com>
2026-05-12 14:03:21 -0700

    Change model source from stabilityai to Manojb

--- seva/modules/autoencoder.py
+++ seva/modules/autoencoder.py
@@ -10,7 +10,7 @@ class AutoEncoder(nn.Module):
     def __init__(self, chunk_size: int | None = None):
         super().__init__()
         self.module = AutoencoderKL.from_pretrained(
-            "stabilityai/stable-diffusion-2-1-base",
+            "Manojb/stable-diffusion-2-1-base",
             subfolder="vae",
             force_download=False,
             low_cpu_mem_usage=False,
